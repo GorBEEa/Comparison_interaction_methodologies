cf3ce4b60e1a6f3781cef0e419f6df1ca1f26141
Christian Gostout <christian.gostout@bc3.net>
2024-12-26 18:27:58 +0100

    End of december update

--- Scripts/Interaction_Data.R
+++ Scripts/Interaction_Data.R
@@ -17,5 +17,41 @@ as_tibble(df.int.trans) #make tibble
 
 #extract Bombus pascuorum interactions
 bpasc.int <- filter(df.int.trans, Polinizador == "Bombus pascuorum")
+df.bpasc <- data.frame(flor.int = bpasc.int$Planta) #make df that is jsut flowers from pascuorum interactions
+
+#clean plant species names
+source("C:/Users/christian.gostout/R/Comparison_interaction_methodologies/Scripts/transectos250_plantas_disponibilid.R") #cleans species names
+source("C:/Users/christian.gostout/R/Comparison_interaction_methodologies/Scripts/transect250_plantastotal (1).R")
+df.bpasc.int <- data.d #cleaned output df
+
+
+## Time to look at what species interactions were observed
+#prep figure features
+title.bpasc.int <- expression(paste("Total ", italic("B. pascuorum"), " interactions with flower species 2023 season")) #new plot, give it a title
+cap.bpasc.int <- expression(paste(bold("Figure X. "), "From April to June, 2023, we surveyed plant-pollinator interactions of 16 transcets in Gorbea Natural Park. Across the field survey season, the bumblebee species,", italic(" B. pascuorum,"), " interacted with X distinct flower species.The most visited flower species was ", italic("Prunella vulgaris.") ))
+
+#make figure
+Figure.X <- ggplot(data = df.bpasc.int, aes(Planta)) + geom_bar() + 
+  labs(x = "Floral species", y = "Interaction count 2023 Season", title = title.bpasc.int, caption = cap.bpasc.int) + 
+  theme(axis.text.x = element_text(angle = 50, vjust = 1, hjust = 1, size = 12),plot.title = element_text(hjust=0.5))
+
+#after several hours I still have no idea how to make a caption wrap acceptably to the figure size
+#obviously that figure is overwhelming
+
+
+
+
+## Focus on flowers with more interactions - make a dataframe only with species with multiple interactions
+
+flor_summary <-table(df.bpasc.int$Planta) #get sums of interactions for 2023 by species
+topflor <- which(flor_summary > 1) #filter out single interaction species
+topflor_df <- data.frame(Species = names(topflor), interactions = as.integer(topflor)) #turn into a nice clean dataframe
+
+#make figure.y
+title.topflor.int <- expression(paste("Top floral species for ", italic("B. pascuorum"), " interactions (2023) ")) #new plot, give it a title
+
+Fig.Y <- ggplot(topflor_df, aes(x = Species, y = interactions)) + geom_bar(stat = "identity") +
+  labs(x = "Floral species", y = "Interaction count 2023 Season", title = title.topflor.int) +
+  theme(axis.text.x = element_text(angle = 50, vjust = 1, hjust = 1, size = 12),plot.title = element_text(hjust=0.5))
 
 
