1c3121d1aa18f55b54affe46dcab98e55e2ee257
csgostout <christian.gostout@bc3research.org>
2025-01-29 17:02:49 +0100

    Poster figures more or less completed

--- Scripts/interactions_x_metabarcoding_2023.R
+++ Scripts/interactions_x_metabarcoding_2023.R
@@ -92,6 +92,14 @@ ggplot(long.gen.by.sites, aes(site, n.genera, fill = method)) +
   scale_fill_manual(values = method.colors) +
   theme(axis.ticks.x = element_blank())
 
+
+#Another basic analysis of diveristy by method (over space and time) ------
+#linear models relating diversity (n.genera) to method*site*period
+
+
+
+
+
 #ok these analyses are interesting for context at least
 #could later go deeper and look at diversity by period across sites
 
@@ -129,7 +137,7 @@ gut.flowers.spp <- mvabund(
   bp23.genomic.analys %>% 
     select(Abelmoschus:last_col())
   )
-mglm.gut.flowers <- manyglm(gut.flowers.spp ~ bp23.genomic.analys$period, family = "")
+#mglm.gut.flowers <- manyglm(gut.flowers.spp ~ bp23.genomic.analys$period, family = "")
 #anova(mglm.gut.flowers, p.uni="adjusted") #this takes a lot of computing power
 #should show deviation and probable significance of effect for each species
 
@@ -140,8 +148,8 @@ mglm.gut.flowers <- manyglm(gut.flowers.spp ~ bp23.genomic.analys$period, family
 #bring together binary presence absence data from interactions and metabarcoding into one table
 
 bp23.all.binary <- full_join(bp23.int4stats.wide.binary, bp23.genomic.binary4stats) %>% 
-  full_join(.,bp23.fc4stats.wide.binary) %>% 
-  replace(is.na(bp23.all.binary), 0)
+  full_join(.,bp23.fc4stats.wide.binary) 
+#bp23.all.binary <- replace(is.na(bp23.all.binary), 0) #this isn't working well. You have to create bp23.all.binary and then recreate with this part
 
 #simplify factors/data involved
 #can do for data with read counts (bp23.genomic.analys) or presence absence (bp23.genomic.binary)
@@ -151,24 +159,107 @@ site <- as.factor(bp23.all.binary$site)
 period <- as.factor(bp23.all.binary$period)
 methodology <- as.factor(bp23.all.binary$method)
 all.flowers <- bp23.all.binary %>% 
-  select(!c(site, period, method)) %>% 
-  replace(is.na(all.flowers), 0)
+  select(!c(site, period, method))  
+all.flowers <- all.flowers %>% replace(is.na(all.flowers), 0)
 
 #NMDS visualiztion
 dist.all.flowers <- vegdist(all.flowers, method = "bray") #calc distance between communities for later stat analysis
 all.flower.mds <- metaMDS(all.flowers, distance = "bray")
 #plot(gut.flower.mds$points, col = site, pch = 16)
-plot(all.flower.mds$points, col = method.colors[as.numeric(method)], pch = 16)
-legend("topleft", legend = levels(method), col = method.colors, pch = 16, title = "Methodology")
+plot(all.flower.mds$points, col = method.colors[as.numeric(methodology)], pch = 16)
+legend("topleft", legend = levels(methodology), col = method.colors, pch = 16, title = "Methodology")
 
-#analysis
+#in ggplot2
+nmds_points <- as.data.frame(all.flower.mds$points)
+colnames(nmds_points) <- c("NMDS1", "NMDS2")
+nmds_points$Methodology <- methodology
+method.colors2 <- c("count" ="slategrey",
+                    "interaction" = "lightblue",
+                    "metabarcoding" = "forestgreen")
+
+polygon_data <- nmds_points %>%
+  group_by(Methodology) %>%
+  slice(chull(NMDS1, NMDS2))
+
+NMDS.title <- expression(paste("NMDS visualization of community composition by methodology"))
+ggplot(nmds_points, aes(x = NMDS1, y = NMDS2, color = Methodology)) +
+  geom_polygon(data = polygon_data, 
+               aes(fill = Methodology, color = NULL), 
+               alpha = 0.2, 
+               show.legend = FALSE) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = method.colors2,
+                     labels = c(
+                      "count" = "Floral diversity survey",
+                      "interaction" = "Interaction observations",
+                      "metabarcoding" = "Metabarcoding detections")) +
+  scale_fill_manual(values = method.colors2) +
+  theme_classic() +  
+  labs(
+    title = NMDS.title,
+    x = "NMDS1",
+    y = "NMDS2",
+    color = "Methodology"
+  ) +
+  theme(plot.title = element_text(hjust=0.5),
+    legend.position = c(0.13, 0.85)
+  )
+
+
+#statistical analysis
 permanova.all.data <- adonis2(all.flowers ~ site*period*methodology, permutations = 9999, method = "bray", by = "terms")
 
 permanova.all.data %>% kbl(caption =
-                             "this is my caption. it is unfortunate that I have to write it here, but oh well." 
+                             "this is my caption!" 
                            ) %>% 
   kable_minimal(full_width = F, html_font = "Cambria")
 
 
 
 
+
+
+
+
+#Poster figure ------
+detects.by.genus <- as.data.frame(colSums(bp23.genomic.binary[12:171])) %>% 
+  rownames_to_column(var = "genus")
+
+detects.comparison <- right_join(detects.by.genus,observed.mb.genus, by = "genus") 
+detects.comparison <- detects.comparison[-161,] %>% 
+  rename(n.sample.detections = "colSums(bp23.genomic.binary[12:171])") %>% 
+  mutate(detected.fc.int = int.detected + flower.count.detected)
+detects.comparison <- detects.comparison[order(detects.comparison$n.sample.detections, decreasing = TRUE) , ] %>% 
+  mutate(color_group = case_when(
+    detected.fc.int == 2 ~ "Both",
+    detected.fc.int == 1 & int.detected == 1 ~ "Int Only",
+    detected.fc.int == 1 & flower.count.detected == 1 ~ "Flower Only",
+    detected.fc.int == 0 ~ "Neither"
+  ))
+
+#select top occurrences
+top.detects.comparison <- detects.comparison[1:31 ,]
+top.detects.comparison <- top.detects.comparison %>% filter(genus != "Symphytum")
+
+#plot
+fig.poster.title <- expression(paste("Top plant genera detected in", italic(" B. pascuorum "), "genetic sampling 2023"))
+fig.poster <- ggplot(top.detects.comparison, aes(x = reorder(genus, -n.sample.detections)
+                                                 , y = n.sample.detections, fill = color_group)) +
+  geom_col(alpha = 0.7) +
+  scale_fill_manual(values = c("Both" = "skyblue4",
+                               "Int Only" = "lightblue",
+                               "Flower Only" = "skyblue2",
+                               "Neither" = "grey80"),
+                    labels = c(
+                      "Both" = "Interactions + diversity survey",
+                      "Int Only" = "Interactions only",
+                      "Flower Only" = "Diversity survey only",
+                      "Neither" = "Metabarcoding only")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(hjust=0.5),
+        legend.position = c(1, 1),
+        legend.justification = c(1, 1)) +
+  labs(x = "Plant Genus", y = "Positive Detections in Gut Samples", fill = "Detection Type") +
+  ggtitle(fig.poster.title) 
+
+fig.poster
